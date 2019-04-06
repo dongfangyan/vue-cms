e3a661bb80d722aa4c0d514b490253536d917ac6
dongfangyan <625612169@qq.com>
2019-04-06 19:10:09 +0800

    修改app和main

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import Vue from '../node_modules/vue/dist/vue.js'
 import app from './App.vue'
 
 import '../lib/mui-master/dist/css/mui.min.css'
-
+import '../lib/mui-master/examples/hello-mui/css/icons-extra.css'
 import { Header } from 'mint-ui';
 
 
